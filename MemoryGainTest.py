38cae67096af405ff3a38f73813765cf294a636a
Joshua Chick <josh.sendemails@gmail.com>
2022-05-17 09:05:07 +1000

    Removed name from program description.

--- MemoryGainTest.py
+++ MemoryGainTest.py
@@ -1,5 +1,5 @@
 """
-Joshua Chick. Memory Gain (c) is a flashcards app.
+Memory Gain (c) is a flashcards app.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
